c3c1d39b1a6709b4fbed95e5e867175bf2ea4773
Russ Perry <rperry1886@gmail.com>
2019-01-31 23:47:15 -0500

    Add guess counter. Refactor code to reduce repeating code.

--- app.js
+++ app.js
@@ -10,31 +10,43 @@ let titleText = document.getElementById("title");
 //Variables
 let max = 1000;
 let min = 1;
-let guess = Math.floor(Math.random() * max) + min;
+let guess = randomNumber();
 let guessCount = 0;
 
-//Functions
-function higher() {
-  min = guess;
-  guess = calculate(min, max);
-  guessCount++;
-  guessNumber.innerHTML = guess;
+/* Function */
+
+//Calculate the guess
+function calculate(min, max) {
+  return Math.floor((min + max) / 2);
 }
-function lower() {
-  max = guess;
-  guess = calculate(min, max);
-  guessCount++;
-  guessNumber.innerHTML = guess;
-}
+//What happens when the guess is right
 function correct() {
   begin.style.display = "inline";
   game.style.display = "none";
   begin.innerHTML = "Play Again";
-  titleText.innerHTML = `I'm a genius! It only took me ${guessCount} tries! Play again?`;
-  min = 1;
-  max = 1000;
-  guess = calculate(min, max);
+  winPhrase(guessCount);
 }
+//What happens when the guess is too low
+function higher() {
+  min = guess;
+  newGuess();
+}
+//What happens when the guess is too high
+function lower() {
+  max = guess;
+  newGuess();
+}
+//Guess again
+function newGuess() {
+  guess = calculate(min, max);
+  guessCount++;
+  guessNumber.innerHTML = guess;
+}
+//Generate random number within the given range
+function randomNumber() {
+  return Math.floor(Math.random() * max) + min;
+}
+//What happens when the play button is pressed
 function startGame() {
   begin.style.display = "none";
   game.style.display = "block";
@@ -43,6 +55,16 @@ function startGame() {
   max += 1;
   guessCount = 0;
 }
-function calculate(min, max) {
-  return Math.floor((min + max) / 2);
+//Generate a phrase for winning based on how many times the game had to guess.
+function winPhrase(guessCount) {
+  min = 1;
+  max = 1000;
+  guess = randomNumber();
+  if (guessCount === 0) {
+    return (titleText.innerHTML = `I'm in your head... I got it right on my first try... Play again?`);
+  } else if (guessCount === 1) {
+    return (titleText.innerHTML = `It only took me one try... Easy.... Play again?`);
+  } else {
+    return (titleText.innerHTML = `I'm a genius! It only took me ${guessCount} tries! Play again?`);
+  }
 }
